6002aff18d03c8cb74615462a12ba973c099d5e2
Dima <playcssv34@gmail.com>
2022-01-28 13:41:09 +0300

    create section aboutme

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,14 @@
+
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      close = document.querySelector('.menu__close');
+menu = document.querySelector('.menu'),
+close = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', function() {
-    menu.classList.add('menu_active');
+menu.classList.add('menu_active');
 });
 
 close.addEventListener('click', function() {
-    menu.classList.remove('menu_active');
+menu.classList.remove('menu_active');
 });
+
+new WOW().init();
