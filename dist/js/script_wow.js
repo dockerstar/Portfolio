b7e282ce164ff6afd199597951102b193df13929
Dima <playcssv34@gmail.com>
2022-01-30 00:27:27 +0300

    create policy and adaptation hamburger and create scrollup

--- dist/js/script_wow.js
+++ dist/js/script_wow.js
@@ -2,8 +2,20 @@ $(window).scroll(function() {
   if ($(this).scrollTop() > 300) {
     $('.two_aside').fadeIn();
     $('.sidepanel').fadeOut();
+    $('.hamburger__black').fadeIn();
+    $('.hamburger').fadeOut();
   } else {
     $('.two_aside').fadeOut();
     $('.sidepanel').fadeIn();
+    $('.hamburger__black').fadeOut();
+    $('.hamburger').fadeIn();
+  }
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1000) {
+    $('.scrollup').fadeIn();
+  } else {
+    $('.scrollup').fadeOut();
   }
 });
--- src/js/script.js
+++ src/js/script.js
@@ -13,12 +13,37 @@ menu.classList.remove('menu_active');
 });
 });
 
+window.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger__black'),
+  menu = document.querySelector('.menu'),
+  close = document.querySelector('.menu__close');
+  
+  hamburger.addEventListener('click', function() {
+  menu.classList.add('menu_active');
+  });
+  
+  close.addEventListener('click', function() {
+  menu.classList.remove('menu_active');
+  });
+  });
+
 const counters = document.querySelectorAll('.percent__value'),
       lines = document.querySelectorAll('.percent__block-show');
 
 counters.forEach( (item, i) => {
   lines[i].style.width = item.innerHTML;
 });
+
+$(document).ready(function(){
+  $(".menu__list, .promo__btns").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
+
+
 
 
 
